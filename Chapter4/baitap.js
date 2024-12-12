2178522fee6aa7a0792cf325f9c4677ce0a98b9c
Bich Ngoc Dev <2ndevdes@gmail.com>
2024-12-12 23:48:16 +0700

    cuoiky

---
+++ Chapter4/baitap.js
@@ -0,0 +1,11 @@
+
+const numbers = [29];
+
+function arrow(arr) {
+    arr.forEach(num => {
+        if (num % 2 === 0) {
+            console.log(number,'La so nguyen')
+        }
+    });
+}
+arrow(numbers);
---
+++ Chapter4/tempCodeRunnerFile.js
@@ -0,0 +1,5 @@
+
+
+const numbers = [1, 2, 3, 4, 5];
+const reversedNumbers = numbers.reverse();
+console.log(reversedNumbers);
